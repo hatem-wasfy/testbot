c5336f5d0f00a3caf9a97364478b072d9e409781
hatem-wasfy <34537566+hatem-wasfy@users.noreply.github.com>
2017-12-14 20:15:42 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php 
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Process only when method is POST
+if($method == 'POST'){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+
+	$text = $json->result->parameters->text;
+
+	switch ($text) {
+		case 'Tokyo':
+			$speech = "Tokyo is a beauiful city, I'll tell you what places to visit there. here we go, check this https://www.google.com/maps/search/places+near+Tokyo";
+			break;
+
+		case 'Fukuoka':
+			$speech = "Fukuoka is a beauiful city, I'll tell you what places to visit there.";
+			break;
+
+		case 'Alexandria':
+			$speech = "Alexandria is a beauiful city, I'll tell you what places to visit there.";
+			break;
+		
+		default:
+			$speech = "Sorry, I didnt get that. Please ask me something else.";
+			break;
+	}
+
+	$response = new \stdClass();
+	$response->speech = $speech;
+	$response->displayText = $speech;
+	$response->source = "webhook";
+	echo json_encode($response);
+}
+else
+{
+	echo "Method not allowed";
+}
+
+?>
