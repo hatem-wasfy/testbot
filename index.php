517e374bf88ecb064468d5390122d3cf575797b1
hatem-wasfy <34537566+hatem-wasfy@users.noreply.github.com>
2017-12-14 20:32:07 +0900

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
-	$text = $json->result->parameters->text;
+	$text = $json->result->parameters->city;
 
 	switch ($text) {
 		case 'Tokyo':
