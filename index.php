41a1fbb1616206cf0d55edc87a4ed0cbb8ac983d
hatem-wasfy <34537566+hatem-wasfy@users.noreply.github.com>
2018-01-04 17:03:34 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,48 +1,31 @@
-<?php 
-
-$method = $_SERVER['REQUEST_METHOD'];
-
-// Process only when method is POST
-if($method == 'POST'){
-	$requestBody = file_get_contents('php://input');
-	$json = json_decode($requestBody);
-
-	$text = $json->result->parameters->city;
-////java script for google maps api/////////////////////////
-	//echo "<script type='text/javascript'>
-          //      alert('JavaScript is awesome!');
-            //</script>";
-///////////////////////////////////////////////////////////
-	
-	switch ($text) {
-		case 'Tokyo':
-			//$speech = "Tokyo is a beauiful city, I'll tell you what places to visit there. Here we go, check this https://www.google.com/maps/search/places+near+Tokyo";
-			$speech = "Tokyo is a beauiful city, I'll tell you what places to visit there. Here we go";
-			
-			break;
-
-		case 'Fukuoka':
-			$speech = "Fukuoka is a beauiful city, I'll tell you what places to visit there.";
-			break;
-
-		case 'Alexandria':
-			$speech = "Alexandria is a beauiful city, I'll tell you what places to visit there.";
-			break;
-		
-		default:
-			$speech = "Sorry, I didnt get that. Please ask me something else.";
-			break;
-	}
-
-	$response = new \stdClass();
-	$response->speech = $speech;
-	$response->displayText = $speech;
-	$response->source = "webhook";
-	echo json_encode($response);
-}
-else
-{
-	echo "Method not allowed";
-}
-
+<?php
+// address to map
+$map_address = "";
+$url = "http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=".urlencode($map_address);
+$lat_long = get_object_vars(json_decode(file_get_contents($url)));
+// pick out what we need (lat,lng)
+$lat_long = $lat_long['results'][0]->geometry->location->lat . "," . $lat_long['results'][0]->geometry->location->lng;
 ?>
+
+<script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+
+<div id="map_canvas"></div>
+			
+<script>
+(function() { 
+function initialize() {
+	var myLatlng = new google.maps.LatLng(<?php echo $lat_long; ?>),
+	mapOptions = {
+		zoom: 15,
+		center: myLatlng
+	},
+	map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions),
+	marker = new google.maps.Marker({
+		position: myLatlng,
+		map: map,
+		title: '<?php echo $map_address; ?>'
+	});
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+})();
+</script>
