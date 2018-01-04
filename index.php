fb7d58fee7c5a2a7fdd7b2588cc2ff43ebc8d4d4
hatem-wasfy <34537566+hatem-wasfy@users.noreply.github.com>
2018-01-04 16:19:53 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php 
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Process only when method is POST
+if($method == 'POST'){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+
+	$text = $json->result->parameters->city;
+////java script for google maps api/////////////////////////
+	//echo "<script type='text/javascript'>
+          //      alert('JavaScript is awesome!');
+            //</script>";
+///////////////////////////////////////////////////////////
+	
+	switch ($text) {
+		case 'Tokyo':
+			//$speech = "Tokyo is a beauiful city, I'll tell you what places to visit there. Here we go, check this https://www.google.com/maps/search/places+near+Tokyo";
+			$speech = "Tokyo is a beauiful city, I'll tell you what places to visit there. Here we go";
+			
+			break;
+
+		case 'Fukuoka':
+			$speech = "Fukuoka is a beauiful city, I'll tell you what places to visit there.";
+			break;
+
+		case 'Alexandria':
+			$speech = "Alexandria is a beauiful city, I'll tell you what places to visit there.";
+			break;
+		
+		default:
+			$speech = "Sorry, I didnt get that. Please ask me something else.";
+			break;
+	}
+
+	$response = new \stdClass();
+	$response->speech = $speech;
+	$response->displayText = $speech;
+	$response->source = "webhook";
+	echo json_encode($response);
+}
+else
+{
+	echo "Method not allowed";
+}
+
+?>
